b8fae5b32e3b1e0ec62eeeb426f0ba4b2ebebb0e
AndrewBnSmith <absmith1058@outlook.com>
2022-06-20 14:00:23 -0400

    added number validation

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int userInput;
 
-        System.out.println("Enter an integer between 1 and 20");
-       int user = scan.nextInt();
+        do {
+            System.out.println("Enter an integer between 1 and 20");
+            userInput = scan.nextInt();
 
-        int[] array = new int[user];
-        int current = 1;
+        } while (userInput > 20 || userInput < 1);
+
+
+        int[] array = new int[userInput];
+        int counter = 1;
         for (int i = 0; i < array.length; i++) {
-            array[i] = current;
-            current *= 2;
+            array[i] = counter;
+            counter *= 2;
             System.out.println(array[i]);
 
         }
+
+        System.out.println(Arrays.toString(array));
 
     }
 
